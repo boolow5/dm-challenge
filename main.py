f37ef1827654f70a17c30ca0677efbb69a12dc63
= <=>
2018-06-27 00:39:05 +0800

    fixed ISSUE #1 script now gets all categories

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ if __name__=='__main__':
     url = 'http://www2.hm.com/en_asia2/index.html'
     page = fetch_page(url)
     woman_categories = parse_woman_link_url(page)
+    if woman_categories == [] or woman_categories is None:
+        print('Failed to parse categories links')
+        exit(1)
     print('found {} sub categories in woman categories'.format(len(woman_categories)))
     lines = ''
     for category in woman_categories:
         # fetch number of items in each category
-        name = find_name_in_category_url(category)
-        count = extract_item_count(category)
-        lines += '{name},{count},{url}\n'.format(name=name, count=count, url=category)
+        # name = find_name_in_category_url(category)
+        if category is None or type(category) is not dict:
+            continue
+        name = category.get('name')
+        url = category.get('url')
+        if name == '' or url == '':
+            continue
+        count = extract_item_count(url)
+        lines += '{name},{count},{url}\n'.format(name=name, count=count, url=url)
     
     print()
     # write to file
@@ -19,4 +28,4 @@ if __name__=='__main__':
     # print(lines)
         
     
-    print("Done!")
+    exit(0)
